eb7c73b56860e269c957f2039360ac68bebf62f5
Troy Thompson <troynt@gmail.com>
2020-12-09 01:57:14 -0500

    slice fix. avoid extra work in loop

--- challenges/9/encoding_error.rb
+++ challenges/9/encoding_error.rb
@@ -6,13 +6,14 @@ require 'pry'
 class EncodingError
   def initialize(lines)
     @nums = lines.map(&:to_i)
+    calc_running_sum!
   end
 
   def calc_part_one(preamble_length = 25)
     @nums.each_with_index do |cur, idx|
       next if idx < preamble_length
 
-      cur_preamble = @nums.slice(idx - preamble_length, idx)
+      cur_preamble = @nums.slice((idx - preamble_length)..idx)
 
       found = cur_preamble.combination(2).find do |a,b|
         cur == a + b
@@ -26,16 +27,22 @@ class EncodingError
     raise "Unable to solve part one."
   end
 
+  def calc_running_sum!
+    sum = 0
+    @running_sum = @nums.map { |x| sum += x }
+  end
+
   def calc_part_two(preamble_length = 25)
     invalid_num = calc_part_one(preamble_length)
 
-    sum = 0
-    running_sum = @nums.map { |x| sum += x }
+    stop_idx = @nums.length - 1
 
-    @nums.each_with_index do |x, end_idx|
-      next if end_idx == 0
-      (end_idx - 1).downto(0).each_with_index do |start_idx|
-        if running_sum[end_idx] - running_sum[start_idx - 1] == invalid_num
+    0.upto(stop_idx).each do |start_idx|
+      (start_idx + 1).upto(stop_idx).each do |end_idx|
+        cur_sum = @running_sum[end_idx] - @running_sum[start_idx - 1]
+        break if cur_sum > invalid_num
+
+        if cur_sum == invalid_num
           return @nums.slice(start_idx..end_idx).minmax.sum
         end
       end
